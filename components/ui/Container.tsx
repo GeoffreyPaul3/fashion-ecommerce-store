50a11a75d9cc82d9f66dd67924461b7cec8e70f5
GeoffreyPaul3 <110961738+Gtigercode@users.noreply.github.com>
2023-11-10 13:20:47 +0200

    Fashion e commerce store repository

---
+++ components/ui/Container.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container:React.FC<ContainerProps> = ({
+    children
+}) => {
+  return (
+    <div className="mx-auto max-w-7xl">
+       {children}
+    </div>
+  )
+}
+
+export default Container;
